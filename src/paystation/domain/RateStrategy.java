4fd506bc126dc0bc0a097c9235334491491e4906
Curtis Kieser <tuh39868@temple.edu>
2019-09-25 05:38:23 -0400

    Update RateStrategy.java
    Created classes for linear rate and progressive rate

--- src/paystation/domain/RateStrategy.java
+++ src/paystation/domain/RateStrategy.java
@@ -14,3 +14,33 @@ public interface RateStrategy {
     public int calculateTime(int moneyInserted);
     
 }
+
+class linearRate implements RateStrategy{
+   int time, money;
+    
+    @Override
+    public int calculateTime(int money){
+        this.time = (money * 2) / 5;
+        return time;
+    }
+            
+}
+
+class progressiveRate implements RateStrategy{
+   int time, money;
+    
+    @Override
+    public int calculateTime(int money){
+        if(money < 150){
+            this.time = (money * 2) / 5;
+        }
+        if(money >= 350){
+            this.time = (money - 350)/5 + 120;
+        }
+        else{
+            this.time = (money - 150)*(3/10) + 60;
+        }
+        return time;
+    }
+            
+}
